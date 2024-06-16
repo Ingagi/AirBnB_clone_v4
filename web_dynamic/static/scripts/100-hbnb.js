8ac746db610032957c56aa582294bd555d6b3644
Ingagi <oshumbusho3@gmail.com>
2024-06-16 14:08:16 -0700

    web dynamic

---
+++ web_dynamic/static/scripts/100-hbnb.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {
+  // Function to fetch places from API based on filters
+  function fetchPlaces(filters) {
+    $.ajax({
+      type: "POST",
+      url: "http://0.0.0.0:5001/api/v1/places_search",
+      contentType: "application/json",
+      data: JSON.stringify(filters),
+      success: function(data) {
+        displayPlaces(data);
+      },
+      error: function(xhr, status, error) {
+        console.error("Error fetching places:", error);
+      }
+    });
+  }
+
+  // Function to display places in the DOM
+  function displayPlaces(places) {
+    var placesHtml = '';
+    places.forEach(function(place) {
+      placesHtml += '<article>';
+      placesHtml += '<div class="title_box">';
+      placesHtml += '<h2>' + place.name + '</h2>';
+      placesHtml += '<div class="price_by_night">$' + place.price_by_night + '</div>';
+      placesHtml += '</div>';
+      placesHtml += '<div class="information">';
+      placesHtml += '<div class="max_guest">' + place.max_guest + ' Guest(s)</div>';
+      placesHtml += '<div class="number_rooms">' + place.number_rooms + ' Bedroom(s)</div>';
+      placesHtml += '<div class="number_bathrooms">' + place.number_bathrooms + ' Bathroom(s)</div>';
+      placesHtml += '</div>';
+      placesHtml += '<div class="description">' + place.description + '</div>';
+      placesHtml += '</article>';
+    });
+    $('.places').html(placesHtml);
+  }
+
+  // Function to handle filter button click
+  $('#filter-btn').click(function() {
+    var filters = {
+      amenities: [],
+      states: [],
+      cities: []
+    };
+
+    // Collect checked amenities
+    $('.amenities input:checked').each(function() {
+      filters.amenities.push($(this).data('id'));
+    });
+
+    // Collect checked states
+    $('.states input:checked').each(function() {
+      filters.states.push($(this).data('id'));
+    });
+
+    // Collect checked cities
+    $('.cities input:checked').each(function() {
+      filters.cities.push($(this).data('id'));
+    });
+
+    // Fetch places based on filters
+    fetchPlaces(filters);
+  });
+
+  // Initial fetch of places on page load (without filters)
+  fetchPlaces({});
+});
+
